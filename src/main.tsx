037f6f6e715ba5e8c145dec94159cdb8770fcb95
Tiziano Cappai <tiziano.cappai1999@gmail.com>
2026-03-17 10:55:38 +0100

    analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import './index.css'
-import { Footer } from './components/Footer.tsx'
-import { Nav } from './components/Nav.tsx'
-import Cometa from './pages/Cometa.tsx'
-import CometaGuide from './pages/CometaGuide.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
+import './index.css';
+import { Footer } from './components/Footer.tsx';
+import { Nav } from './components/Nav.tsx';
+import Cometa from './pages/Cometa.tsx';
+import CometaGuide from './pages/CometaGuide.tsx';
+import { Analytics } from '@vercel/analytics/next';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <div className="flex min-h-screen flex-col">
-        <Nav />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Cometa />} />
-            <Route path="/cometa" element={<Cometa />} />
-            <Route path="/cometa-guide" element={<CometaGuide />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  </StrictMode>,
-)
+	<StrictMode>
+		<BrowserRouter>
+			<div className='flex min-h-screen flex-col'>
+				<Nav />
+				<main className='flex-1'>
+					<Routes>
+						<Route path='/' element={<Cometa />} />
+						<Route path='/cometa' element={<Cometa />} />
+						<Route path='/cometa-guide' element={<CometaGuide />} />
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+			<Analytics />
+		</BrowserRouter>
+	</StrictMode>,
+);
